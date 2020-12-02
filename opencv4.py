33b448662465c1dc537043fced54be052888029a
kaunghtethtun <kaunghtethtun2013@gmail.com>
2020-12-03 00:51:15 +0630

    add&modify new file

--- opencv4.py
+++ opencv4.py
@@ -1,4 +1,5 @@
 import cv2
+import datetime
 
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
@@ -8,9 +9,13 @@ print(cap.get(4))
 
 while(True):
     ret,vd = cap.read()
+    # put text in image show
     font = cv2.FONT_HERSHEY_COMPLEX
+    datet = str(datetime.datetime.now())
     text = 'width: '+ str(cap.get(3))+',height: '+str(cap.get(4))
-    vd = cv2.putText(vd,text,(10,100),font,3,(50,50,200),3,cv2.LINE_AA)
+    vd = cv2.putText(vd,datet,(10,100),font,3,
+                        (50,50,200),2,cv2.LINE_8)
+    # put text in image show
     cv2.imshow("Testing",vd)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
